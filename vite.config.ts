699a3ed09bfbcaa01cd1940ed45b81177c26b27c
diprana416 <diprana416@gmail.com>
2025-09-25 07:31:42 +0530

    minor fixing

--- vite.config.ts
+++ vite.config.ts
@@ -11,13 +11,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://api.savitarrealty.com",
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
